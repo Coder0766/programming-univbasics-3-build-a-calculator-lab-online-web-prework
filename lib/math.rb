47708d64a36b69ff1c6236c0f61703d0bd158448
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-27 19:16:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,11 +8,11 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
- division = num1 + num2
+ quotient = num1 + num2
 
 
 def multiplication(num1, num2)
- sum = num1 + num2
+ product = num1 + num2
 end
 
 def modulo(num1, num2)
