031179dac402974f5870bdc3cdf870813f2a3f7c
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-28 00:34:02 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,1 +1,29 @@
- 
+ def addition(num1, num2)
+  sum = (5 + 4)
+  puts sum
+end
+
+def subtraction(num1, num2)
+ difference = (5- 4)
+ puts difference
+end
+
+def division(num1, num2)
+ quotient = (5 / 4)
+ puts quotient
+end
+
+def multiplication(num1, num2)
+ product = (5 * 4)
+ puts product
+end
+
+def modulo(num1, num2)
+ remainder = (5 % 4)
+  puts remainder
+end
+
+def square_root(num^)
+ square = (num)
+ puts square
+end
