24ca21c0a3c111809ef8e37767cb8226e40d38cc
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-28 00:55:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,28 +1,23 @@
- def addition(num1, num2)
+def addition(num1, num2)
   sum = (num1 + num2)
 end
 
 def subtraction(num1, num2)
  difference = (num1 - num2)
- puts difference
 end
 
 def division(num1, num2)
  quotient = (num1 / num2)
- puts quotient
 end
 
 def multiplication(num1, num2)
  product = (num1 * num2)
- puts product
 end
 
 def modulo(num1, num2)
  remainder = (num1 % num2)
-  puts remainder
 end
 
 def math.sqrt(num)
  math.sqrt = (num)
- puts math.sqrt
 end
